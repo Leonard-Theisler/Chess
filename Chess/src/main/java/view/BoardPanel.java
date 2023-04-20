5d999ed910e19d8c669eacdb19146fbca68ce244
Leonard-Theisler <s204688@dtu.dk>
2023-04-20 16:57:10 +0200

    Forgot to branch, starting the GUI

---
+++ Chess/src/main/java/view/BoardPanel.java
@@ -0,0 +1,18 @@
+package view;
+
+import java.awt.Dimension;
+
+import Setup.Board;
+
+public class BoardPanel {
+	private int rows = 8;
+	private int cols = 8;
+	private int maxDimension = 500;
+	int cellWidth = maxDimension/rows;
+
+	public BoardPanel(Board board) {
+		
+		
+		
+	}
+}
